bec95e4769201bb2940fb53c7ef019e714835366
eman hamid <emanhharki@gmail.com>
2021-06-08 17:48:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MovieShowcase from './MovieShowcase';
-
-ReactDOM.render(<MovieShowcase />, document.getElementById('root'));
